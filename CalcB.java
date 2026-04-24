031e063b27a648f191549e8ce5fd758a7d67b7ef
Santiago Per <persantiago3@gmail.com>
2026-04-24 10:45:15 -0600

    Add files via upload

---
+++ CalcB.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.calcb;
+
+/**
+ *
+ * @author santiagoandre
+ */
+public class CalcB {
+    public static void main(String[] args) {
+          NewJFrame holapos=new NewJFrame ();
+            holapos.setVisible(true);
+    }
+
+    }
